07b28c7446bfd1623ac3e8176e0f3d439cf943a3
Fabrizio Montemayor <20170994@aloe.ulima.edu.pe>
2022-07-02 00:57:19 -0500

    [fix] use ssl for production

--- src/database-connection.service.ts
+++ src/database-connection.service.ts
@@ -20,6 +20,7 @@ export class DatabaseConnectionService implements TypeOrmOptionsFactory {
       dropSchema: false,
       logging: true,
       entities: ['dist/**/*.entity.js'],
+      ssl: process.env.NODE_ENV === 'production',
     };
   }
 }
